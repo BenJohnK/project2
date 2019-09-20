465eff69b99ed300c4fa8a7f61b99bb21192ee58
Navaneeth <nksanthosh@suyati.com>
2019-09-20 13:09:50 +0530

    first commit

---
+++ foundation/2.    Asp.Net MVC, Asp.Net – Web Application Development/C#/C#-1/sampleapp/sampleapp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleapp
+{
+    class TestTemperature
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature();
+            DividebyZero Sample = new DividebyZero();
+            try
+            {
+                Sample.show();
+                temp.showTemp();
+            }
+            catch (TempIsZeroException e)
+            {
+                Console.WriteLine("TempIsZeroException: {0} -- {1}", e.Message, e.Source);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+public class TempIsZeroException : Exception
+{
+    public TempIsZeroException(string message)
+        : base(message)
+    {
+    }
+}
+
+public class Temperature
+{
+    int temperature = 0;
+    public void showTemp()
+    {
+        if (temperature == 0)
+        {
+            throw (new TempIsZeroException("Zero Temperature found"));
+        }
+        else
+        {
+            Console.WriteLine("Temperature: {0}", temperature);
+        }
+    }
+}
---
+++ foundation/2.    Asp.Net MVC, Asp.Net – Web Application Development/C#/C#-1/sampleapp/sampleapp/Sample1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleapp
+{
+    class DividebyZero
+    {
+        public int abc;
+        private int[] j = new int[] { 1, 2 };
+        private int[] g = new int[2];
+        private int a = 2, b = 0, c;
+
+        public void show()
+        {
+            try
+            {
+                c = a / b;
+                c = g[5];
+            }
+            catch (IndexOutOfRangeException w)
+            {
+                Console.WriteLine("index - " + w.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(1+"catch" + e.Message);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+            finally
+            {
+                Console.WriteLine("END");
+            }
+        }
+    }
+}
